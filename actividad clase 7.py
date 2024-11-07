e1104c23916dbc57d053b5f92b8752459519258d
Renzo-Bazan <renzobazanaguirres@gmail.com>
2024-11-06 22:19:25 -0300

    Actividad clase 7.py

--- actividad clase 7.py
+++ actividad clase 7.py
@@ -144,8 +144,16 @@ harry_potter = Personaje("Humano", True, 17)
 
 # Práctica Métodos 1
 # Crea una clase Perro. Dicho perro debe poder ladrar.
+class Perro:
+    def __init__(self):
+        pass
+    
+    def ladrar (self):
+        print("Guau")
 
 # Crea el método ladrar() y ejecútalo en una instancia de Perro. Cada vez que ladre, debe mostrarse en pantalla "Guau!".
+
+
 
 
 # Práctica Métodos 2
@@ -153,6 +161,17 @@ harry_potter = Personaje("Humano", True, 17)
 
 # Crea una instancia de Mago en el objeto merlin, y haz que lance un hechizo.
 
+class Mago:
+    def __init__(self):
+        pass
+    
+    def lanzar_hechizo(self):
+        print("Abracadabra")
+
+merlin = Mago()
+
+merlin = Mago()
+merlin.lanzar_hechizo()
 
 
 # Práctica Métodos 3
@@ -160,7 +179,15 @@ harry_potter = Personaje("Humano", True, 17)
 
 # "La alarma ha sido pospuesta {cantidad_minutos} minutos"
 
+class Alarma:
+    def __init__(self):
+        pass
 
+    def postergar(self,cantidad_minutos):
+        print(f"La alarma ha sido pospuesta {cantidad_minutos} minutos")
+
+mi_alarma = Alarma
+mi_alarma.postergar(10)
 
 # class Pajaro():
 
@@ -208,16 +235,47 @@ harry_potter = Personaje("Humano", True, 17)
 # Práctica Tipos de Métodos 1
 # Crea un método estático respirar() para la clase Mascota. Cuando se llame, debe imprimir en pantalla "Inhalar... Exhalar"
 
+class Mascota:
+    @staticmethod
+    def respirar():
+        print("Inhalar... Exhalar")
 
 
 # Práctica Tipos de Métodos 2
 # Crea un método de clase revivir() que actúa sobre el atributo de clase vivo de la clase Jugador, estableciéndolo en True cada vez que es invocado. El valor predeterminado del atributo vivo, debe ser False.
 
+class Jugador:
+    # Atributo de clase
+    vivo = False
 
+    @classmethod
+    def revivir(cls):
+        cls.vivo = True
+        print("El jugador ha revivido y está vivo.")
+
+print(Jugador.vivo)
+
+Jugador.revivir()
+
+print(Jugador.vivo)
+
+    
 
 # Práctica Tipos de Métodos 3
 # Crea un método de instancia lanzar_flecha() que reste en -1 la cantidad de flechas que tiene una instancia de Personaje, que cuenta con un atributo de instancia de tipo número, llamado cantidad_flechas.
 
+class Personaje:
+    def __init__(self, cantidad_flechas):
+        self.cantidad_flechas = cantidad_flechas
+
+    def lanzar_flecha(self):
+        self.cantidad_flechas = self.cantidad_flechas - 1
+
+mi_personaje = Personaje(100)
+
+mi_personaje.lanzar_flecha()
+
+print(mi_personaje.cantidad_flechas)
 
 
 #-----------------------------------------------------------------------------------------
@@ -251,14 +309,42 @@ harry_potter = Personaje("Humano", True, 17)
 # Práctica Herencia 1
 # Crea una clase llamada Persona, que tenga los siguientes atributos de instancia: nombre, edad. Crea otra clase, Alumno, que herede de la primera estos atributos.
 
+class Persona:
+    def __init__(self,nombre,edad):
+        self.nombre = nombre
+        self.edad = edad
+
+class Alumno(Persona):
+    pass
+    
+
 
 # Práctica Herencia 2
 # Crea una clase llamada Mascota, que tenga los siguientes atributos de instancia: edad, nombre, cantidad_patas. Crea otra clase, Perro, que herede de la primera sus atributos
 
+class Mascota:
+    def __init__(self,edad,nombre,cantidad_patas):
+        self.edad = edad
+        self.nombre = nombre
+        self.cantidad_patas = cantidad_patas
+
+class Perro(Mascota):
+    pass
 
 
 # Práctica Herencia 3
 # Crea una clase llamada Vehiculo, que contenga los métodos acelerar() y frenar() (puedes dejar el código de los métodos en blanco con pass). Crea una clase llamada Automovil que herede estos métodos de Vehiculo.
+
+class Vehiculo:
+    def acelerar(self):
+        pass
+    
+    def frenar(self):
+        pass
+    
+class Automovil(Vehiculo):
+    pass
+
 
 
 #------------------------------------------------------------------------------
@@ -347,26 +433,27 @@ harry_potter = Personaje("Humano", True, 17)
 # Completa el código suministrado a continuación para lograrlo
 
 
-# class Padre():
-    # def trabajar(self):
-        # print("Trabajando en el Hospital")
+class Padre():
+    def trabajar(self):
+        print("Trabajando en el Hospital")
 
-    # def reir(self):
-        # print("Ja ja ja!")
+    def reir(self):
+        print("Ja ja ja!")
 
-# class Madre():
-    # def trabajar(self):
-        # print("Trabajando en la Fiscalía")
+class Madre():
+   def trabajar(self):
+        print("Trabajando en la Fiscalía")
 
-# class Hija(COMPLETAR):
-    # COMPLETAR
+class Hija(Padre, Madre):
+    def mostrar_trabajo(self):
+        Madre.trabajar(self)
+    
+    def risa(self):    
+        Padre.reir(self)
+        
+hija = Hija()      
+    
 
-
-
-# Práctica Herencia Extendida 1
-# Si la clase Hija ha heredado de su padre la forma de reir, y de su madre la vocación, y hoy tienen el mismo trabajo en la Fiscalía, crea la herencia múltiple que le permita a esta clase heredar correctamente de Padre y Madre.
-
-# Completa el código suministrado a continuación para lograrlo
 
 # Práctica Herencia Extendida 2
 # "El ornitorrinco es una de las criaturas más raras del mundo: aunque es un mamífero, pone huevos; y amamanta a sus crías pero no tienen mamas." (National Geographic)
@@ -388,31 +475,45 @@ harry_potter = Personaje("Humano", True, 17)
 # - amamantar()
 
 
-# class Vertebrado:
-    # vertebrado = True
+class Vertebrado:
+    vertebrado = True
 
-# class Ave(Vertebrado):
-    # tiene_pico = True
-    # def poner_huevos(self):
-        # print("Poniendo huevos")
+class Ave(Vertebrado):
+    tiene_pico = True
+    def poner_huevos(self):
+        print("Poniendo huevos")
 
-# class Reptil(Vertebrado):
-    # venenoso = True
+class Reptil(Vertebrado):
+    venenoso = True
 
-# class Pez(Vertebrado):
-    # def nadar(self):
-        # print("Nadando")
-    # def poner_huevos(self):
-        # print("Poniendo huevos")
+class Pez(Vertebrado):
+    def nadar(self):
+        print("Nadando")
+    def poner_huevos(self):
+        print("Poniendo huevos")
 
-# class Mamifero(Vertebrado):
-    # def caminar(self):
-        # print("Caminando")
-    # def amamantar(self):
-        # print("Amamantando crías")
+class Mamifero(Vertebrado):
+    def caminar(self):
+        print("Caminando")
+    def amamantar(self):
+        print("Amamantando crías")
 
-# class Ornitorrinco(COMPLETAR):
-#     COMPLETAR
+class Ornitorrinco(Vertebrado,Ave,Reptil,Pez,Mamifero):
+    def caracteristicas_aves(self):
+        Ave.tiene_pico(self)
+        Ave.poner_huevos(self)
+
+    def huesos(self):
+        Vertebrado(self)
+    
+    def veneno(self):
+        Reptil.venenoso(self)
+    
+    def nadar(self):
+        Pez.nadar(self)
+        
+
+
 
 # Práctica Herencia Extendida 3
 # Un hijo ha heredado de su padre todas sus características, sin embargo, tienen diferentes hobbies. Logra que la clase Hijo herede de Padre todos sus métodos y atributos, sobreescribiendo el método hobby() para que se devuelva[1]: "Juego videojuegos en mi tiempo libre"
